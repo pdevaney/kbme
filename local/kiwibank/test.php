846bae6b890b53366911e9cf863667452e4f587c
peterdevaney <peterdevaney@192.168.0.132>
2021-02-18 18:25:44 +1300

    Change php library used for sftp (2)

--- local/kiwibank/test.php
+++ local/kiwibank/test.php
@@ -3,9 +3,9 @@
 global $CFG;
 
 
-require ('/var/www/staging-kbme.kiwibank.co.nz/local/kiwibank/classes/totara_upload.php');
+require ('/var/www/staging-kbme.kiwibank.co.nz/local/kiwibank/classes/totara_sync.php');
 
-$loader= new totara_sync_feedupload();
+$loader= new totara_sync();
 $loader->upload_feedfiles();
 
 
